c173d8da346bf657292be0047d3abc6a6989482d
PaoloDeMori <150668896+PaoloDeMori@users.noreply.github.com>
2025-01-28 03:12:44 +0100

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,6 +4,9 @@ import textFixer as tF
 import sys
 
 color = "snow3"
+size = "1400x650"
+standardPadding = 15
+standardFont= ("arial",10) 
 global isSpecialDeleterOn
 global isFLSpaceDeleterOn
 global isSpaceDeleterOn
@@ -55,7 +58,7 @@ def mainWindowBuilder(specialChars):
     isFLSpaceDeleterOn=tkt.BooleanVar()
     global isSpecialDeleterOn
     isSpecialDeleterOn=tkt.BooleanVar()
-    mainWindow.grid(row=0,column=1,sticky="ns",padx=15,pady=15)
+    mainWindow.grid(row=0,column=1,sticky="ns",padx=standardPadding,pady=standardPadding)
     titleLabel=tkt.Label(mainWindow, text=title, font=("Arial",20))
     titleLabel.pack(pady=25)
     optionFrame=tkt.Frame(mainWindow)
@@ -63,45 +66,45 @@ def mainWindowBuilder(specialChars):
     optionFrame.pack(pady=25)
 
     labelOption1 = tkt.Label(optionFrame, text="Caratteri Considerati Speciali")
-    labelOption1.grid(row=0, column=0,padx=15,pady=15)
+    labelOption1.grid(row=0, column=0,padx=standardPadding,pady=standardPadding)
     global labelOption2
     labelOption2 = tkt.Label(optionFrame, text=specialChars,font=("arial",19))
-    labelOption2.grid(row=0, column=1,padx=15,pady=15)
+    labelOption2.grid(row=0, column=1,padx=standardPadding,pady=standardPadding)
 
     global entryOption1
     entryOption1 = tkt.Entry(optionFrame,font=("arial",12))
-    entryOption1.grid(row=1,column=0,padx=15,pady=15)
+    entryOption1.grid(row=1,column=0,padx=standardPadding,pady=standardPadding)
     entryButton1 = tkt.Button(optionFrame,text="Aggiungi Un Carattere Speciale", command=modifyChars)
-    entryButton1.grid(row=1,column=1,padx=15,pady=15)
+    entryButton1.grid(row=1,column=1,padx=standardPadding,pady=standardPadding)
 
 
     textsFrame=tkt.Frame(mainWindow)
     textsFrame.config(bg=color)
     textsFrame.pack(pady=25)
-    label1 = tkt.Label(textsFrame, text="Inserisci Qui Il testo Da Modificare",font=("arial",10))
-    label1.grid(row=0, column=1,padx=15,pady=15)
-    label1 = tkt.Label(textsFrame, text="Testo modificato",font=("arial",10))
-    label1.grid(row=0, column=3,padx=15,pady=15)
+    label1 = tkt.Label(textsFrame, text="Inserisci Qui Il testo Da Modificare",font=standardFont)
+    label1.grid(row=0, column=1,padx=standardPadding,pady=standardPadding)
+    label1 = tkt.Label(textsFrame, text="Testo modificato",font=standardFont)
+    label1.grid(row=0, column=3,padx=standardPadding,pady=standardPadding)
 
     global textField1
-    textField1 = tkt.Text(textsFrame,font=("arial",10), height=30, width=50)
+    textField1 = tkt.Text(textsFrame,font=standardFont, height=standardPadding, width=50)
     scrollbar1 = tkt.Scrollbar(textsFrame,orient="vertical", command=textField1.yview)
     textField1.config(yscrollcommand=scrollbar1.set)
     scrollbar1.grid(row=1, column=0, sticky="ns")
-    textField1.grid(row=1, column=1,padx=(15,0),pady=15)
+    textField1.grid(row=1, column=1,padx=(standardPadding,0),pady=standardPadding)
 
-    entryButton2 = tkt.Button(textsFrame,text="-->", command=modifyText,font=("arial",10))
-    entryButton2.grid(row=1,column=2,padx=15)
+    entryButton2 = tkt.Button(textsFrame,text="-->", command=modifyText,font=standardFont)
+    entryButton2.grid(row=1,column=2,padx=standardPadding)
 
     global textField2
-    textField2 = tkt.Text(textsFrame,font=("arial",10), height=30, width=50)
+    textField2 = tkt.Text(textsFrame,font=standardFont, height=standardPadding, width=50)
     scrollbar2 = tkt.Scrollbar(textsFrame,orient="vertical", command=textField2.yview)
     textField2.config(yscrollcommand=scrollbar2.set)
-    textField2.grid(row=1, column=3,padx=(0,15),pady=15)
+    textField2.grid(row=1, column=3,padx=(0,standardPadding),pady=standardPadding)
     scrollbar2.grid(row=1, column=4, sticky="ns")
 
-    entryButton3 = tkt.Button(textsFrame,text="Copia", command=copyText,font=("arial",10))
-    entryButton3.grid(row=1,column=5,padx=15)
+    entryButton3 = tkt.Button(textsFrame,text="Copia", command=copyText,font=standardFont)
+    entryButton3.grid(row=1,column=5,padx=standardPadding)
 
 def checkBoxCreator():
     checkBoxFrame=tkt.Frame(root)
@@ -111,7 +114,7 @@ def checkBoxCreator():
     checkBox2.pack(anchor="w")
     checkBox3=tkt.Checkbutton(checkBoxFrame,text="Attivare l'eliminazione dei caratteri speciali?",variable=isSpecialDeleterOn,onvalue=True,offvalue=False)
     checkBox3.pack(anchor="w")
-    checkBoxFrame.grid(row=0,column=0,sticky="ns",padx=15,pady=15)
+    checkBoxFrame.grid(row=0,column=0,sticky="ns",padx=standardPadding,pady=standardPadding)
 
 
 
@@ -125,7 +128,7 @@ def startGui(specialChars):
     root = tkt.Tk()
     root.title(title)
     root.config(bg=color)
-    root.geometry("1400x900")
+    root.geometry(size)
     mainWindowBuilder(specialChars)
     checkBoxCreator()
     
